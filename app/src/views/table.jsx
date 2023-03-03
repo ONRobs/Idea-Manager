34a3dc665ee29651b9b87986da6e2565d9f33b45
ONRobs <75011517+ONRobs@users.noreply.github.com>
2023-03-03 23:49:38 +0200

    Implemetēti "search" lauki.

--- app/src/views/table.jsx
+++ app/src/views/table.jsx
@@ -7,29 +7,33 @@ export default function Table() {
     const [popupData, setPopupData] = useState([]);
     const [visibility, setVisibility] = useState(true);
     const [tags, setTags] = useState("")
+    const [searchIdea, setSearchIdea] = useState("");
+    const [searchTags, setSearchTags] = useState("");
+    const [filteredIdea, setFilteredIdea] = useState([]);
 
     // iegūst datus no servera
     useEffect(() => {
         axios.get('http://localhost:3004/ideas')
             .then(response => {
                 setIdea(response.data);
+                setFilteredIdea(response.data);
             })
             .catch(error => {
                 console.log(error);
             });
     }, []);
-    
+
     // formatē Tag vērtības, lai tās būtu atdalītas ar ", " popup logā
     useEffect(() => {
         if (popupData && popupData.Tags) {
           setTags(popupData.Tags.split(",").map(tag => tag.trim()).join(", "));
         }
-    }, [popupData]);
-      
+    }, [popupData, filteredIdea]);
+
     // atver popup logu attiecīgajai idejai
     const handleClick = event => {
         const ideaId = event.target.parentNode.dataset.ideaId;
-        const clickedIdea = idea.find(idea => idea.Idea_ID === parseInt(ideaId));
+        const clickedIdea = filteredIdea.find(idea => idea.Idea_ID === parseInt(ideaId));
         setPopupData(clickedIdea);
         setVisibility(false);
     }
@@ -40,7 +44,34 @@ export default function Table() {
         setPopupData([])
         setVisibility(true)
     }
-    
+
+    // filtrē ideju sarakstu pēc meklēšanas vērtībām
+    useEffect(() => {
+        let newFilteredIdea = idea;
+        if (searchIdea !== "") {
+            newFilteredIdea = newFilteredIdea.filter(
+                idea => idea.Idea.toLowerCase().includes(searchIdea.toLowerCase())
+            );
+        }
+        if (searchTags !== "") {
+            newFilteredIdea = newFilteredIdea.filter(
+                idea => idea.Tags.toLowerCase().includes(searchTags.toLowerCase())
+            );
+        }
+
+        setFilteredIdea(newFilteredIdea);
+    }, [idea, searchIdea, searchTags]);
+
+    // atbild par Idea meklēšanas ievadlauka izmaiņām
+    const handleSearchChange = event => {
+        setSearchIdea(event.target.value);
+    };
+
+    // atbild par Tag meklēšanas ievadlauka izmaiņām
+    const handleTagSearchChange = event => {
+        setSearchTags(event.target.value);
+    };
+
     return(
         <>
             {/* popup logs */}
@@ -57,10 +88,22 @@ export default function Table() {
             <div className="block-table">
                 <form>
                     <div className="input-block">
-                        {/* idejas meklēšanas lauks */}
-                        <input name="search-idea" placeholder="Search for an Idea:" autoComplete="off" />
+                        {/* Idea meklēšanas lauks */}
+                        <input 
+                            name="search-idea" 
+                            placeholder="Search for an Idea:" 
+                            autoComplete="off"
+                            value={searchIdea}
+                            onChange={handleSearchChange}
+                        />
                         {/* Tag meklēšanas lauks */}
-                        <input name="search-tags" placeholder="Search for Tags:" autoComplete="off" />
+                        <input 
+                            name="search-tags" 
+                            placeholder="Search for Tags:" 
+                            autoComplete="off" 
+                            value={searchTags}
+                            onChange={handleTagSearchChange}
+                        />
                     </div>
                 </form>
 
@@ -76,17 +119,17 @@ export default function Table() {
                     </thead>
                     {/* tabulas rindas */}
                     <tbody>
-                        {idea.map(idea => (
+                        {filteredIdea.map(idea => (
                             <tr key={idea.Idea_ID} data-idea-id={idea.Idea_ID}>
-                            <td onClick={handleClick}>{idea.Idea}</td>
-                            <td>{idea.Tags.split(",").map(tag => tag.trim()).join(", ")}</td>
-                            <td>{idea.Date_Created}</td>
-                            <td>{idea.Rating}</td>
+                                <td onClick={handleClick}>{idea.Idea}</td>
+                                <td>{idea.Tags.split(",").map(tag => tag.trim()).join(", ")}</td>
+                                <td>{idea.Date_Created}</td>
+                                <td>{idea.Rating}</td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
         </>
-    )
+    );
 }
