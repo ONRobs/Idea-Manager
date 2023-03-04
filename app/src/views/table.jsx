111a8fb2dbaedd7ed036310081bbf96f925a0670
ONRobs <75011517+ONRobs@users.noreply.github.com>
2023-03-04 11:54:02 +0200

    Pievienots table sorting pēc Idea, Date_Created un Rating

--- app/src/views/table.jsx
+++ app/src/views/table.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react"
+import Trash from '../assets/Trash.svg';
+import Edit from '../assets/Edit.svg';
 import Cross from '../assets/Cross.svg';
 import axios from 'axios';
 
 export default function Table() {
     const [idea, setIdea] = useState([]);
-    const [popupData, setPopupData] = useState([]);
-    const [visibility, setVisibility] = useState(true);
     const [tags, setTags] = useState("")
+    // popup logam
+    const [visibility, setVisibility] = useState(true);
+    const [popupData, setPopupData] = useState([]);
+    // meklēšanas laukiem
     const [searchIdea, setSearchIdea] = useState("");
     const [searchTags, setSearchTags] = useState("");
     const [filteredIdea, setFilteredIdea] = useState([]);
+    // tabulas kārtošanai
+    const [sort, setSort] = useState(null);
 
     // iegūst datus no servera
     useEffect(() => {
@@ -72,12 +78,50 @@ export default function Table() {
         setSearchTags(event.target.value);
     };
 
+    // atbild par lauku kārtošanu tabulā
+    const sortIdeaList = () => {
+        const sortedIdea = [...filteredIdea];
+        if (sort) {
+            sortedIdea.sort((a, b) => {
+                const columnA = a[sort.column];
+                const columnB = b[sort.column];
+                if (columnA < columnB) {
+                    return sort.direction === 'asc' ? -1 : 1;
+                } else if (columnA > columnB) {
+                    return sort.direction === 'asc' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+        return sortedIdea;
+    }
+    
+    // atbild par to, pēc kura lauka tiek kārtots
+    const handleSort = (column) => {
+        const newSort = {
+            column,
+            direction: sort && sort.direction === 'asc' ? 'desc' : 'asc',
+        };
+        setSort(newSort);
+    }
+      
+    // izmanto atgriezto sakārtoto tabulu
+    useEffect(() => {
+        setFilteredIdea(sortIdeaList());
+    }, [idea, searchIdea, searchTags, sort]);
+      
+
     return(
         <>
             {/* popup logs */}
             <div className={visibility ? 'popup-screen' : 'popup-screen visible'}>
                 <div className="block-form">
-                    <img src={Cross} alt="" className="cross" onClick={handleClose} />
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <img src={Cross} alt="" className="cross" onClick={handleClose} />
+                    </div>
                     <div className="heading sub-heading bold-italic">{popupData.Idea}</div>
                     <p className="text">{popupData.Description}</p>
                     <div className="text">Tags: {tags}</div>
@@ -106,15 +150,14 @@ export default function Table() {
                         />
                     </div>
                 </form>
-
                 <table>
                     {/* tabulas kolonnu virsraksti */}
                     <thead>
                         <tr>
-                            <th>Idea</th>
+                            <th onClick={() => handleSort('Idea')}>Idea</th>
                             <th>Tags</th>
-                            <th>Date</th>
-                            <th>Rating</th>
+                            <th onClick={() => handleSort('Date_Created')}>Date</th>
+                            <th onClick={() => handleSort('Rating')}>Rating</th>
                         </tr>
                     </thead>
                     {/* tabulas rindas */}
